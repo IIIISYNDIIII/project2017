e6acebf5c286f725528d2215bde235db46daed63
IIIISYNDIIII <105109077+IIIISYNDIIII@users.noreply.github.com>
2022-05-07 14:25:32 +0300

    Add files via upload
    v1.0

---
+++ maze.py
@@ -0,0 +1,128 @@
+from pygame import *
+'''Необходимые классы'''
+ 
+#класс-родитель для спрайтов
+class GameSprite(sprite.Sprite):
+   #конструктор класса
+   def __init__(self, player_image, player_x, player_y, player_speed):
+       super().__init__()
+       # каждый спрайт должен хранить свойство image - изображение
+       self.image = transform.scale(image.load(player_image), (55, 55))
+       self.speed = player_speed
+       # каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
+       self.rect = self.image.get_rect()
+       self.rect.x = player_x
+       self.rect.y = player_y
+ 
+   def reset(self):
+       window.blit(self.image, (self.rect.x, self.rect.y))
+ 
+#класс-наследник для спрайта-игрока (управляется стрелками)
+class Player(GameSprite):
+   def update(self):
+       keys = key.get_pressed()
+       if keys[K_LEFT] and self.rect.x > 5:
+           self.rect.x -= self.speed
+       if keys[K_RIGHT] and self.rect.x < win_width - 80:
+           self.rect.x += self.speed
+       if keys[K_UP] and self.rect.y > 5:
+           self.rect.y -= self.speed
+       if keys[K_DOWN] and self.rect.y < win_height - 80:
+           self.rect.y += self.speed
+ 
+#класс-наследник для спрайта-врага (перемещается сам)
+class Enemy(GameSprite):
+   def update(self):
+       if self.rect.x <= 470:
+           self.side = "right"
+       if self.rect.x >= win_width - 85:
+           self.side = "left"
+       if self.side == "left":
+           self.rect.x -= self.speed
+       else:
+           self.rect.x += self.speed
+
+class Wall(sprite.Sprite):
+    def __init__(self, color_1, color_2, color_3, wall_x, wall_y, wall_widht, wall_height):
+        super().__init__()
+        self.color_1 = color_1
+        self.color_2 = color_2
+        self.color_3 = color_3
+        self.width = wall_widht
+        self.height = wall_height
+        self.image = Surface((self.width, self.height))
+        self.image.fill((color_1, color_2, color_3))
+        self.rect = self.image.get_rect()
+        self.rect.x = wall_x
+        self.rect.y = wall_y
+    def draw_wall(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+ 
+#Игровая сцена:
+win_width = 700
+win_height = 500
+window = display.set_mode((win_width, win_height))
+display.set_caption("Maze")
+background = transform.scale(image.load("background.jpg"), (win_width, win_height))
+ 
+#Персонажи игры:
+player = Player('hero.png', 5, win_height - 80, 4)
+monster = Enemy('cyborg.png', win_width - 80, 280, 2)
+final = GameSprite('treasure.png', win_width - 120, win_height - 80, 0)
+wall_1 = Wall(255, 255, 255, 125, 100, 50, 700)
+wall_2 = Wall(255, 255, 255, 275, -300, 50, 700)
+wall_3 = Wall(255, 255, 255, 425, 100, 50, 700)
+wall_4 = Wall(255, 255, 255, 575, -300, 50, 700)
+
+
+ 
+ 
+ 
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+ 
+font.init()
+font = font.Font(None, 70)
+win = font.render('Ты натурал!', True, (255, 215, 0))
+lose = font.render('Ты', True, (180, 0, 0))
+ 
+#музыка
+mixer.init()
+mixer.music.load('jungles.ogg')
+mixer.music.play()
+ 
+money = mixer.Sound('money.ogg')
+kick = mixer.Sound('kick.ogg')
+ 
+while game:
+   for e in event.get():
+       if e.type == QUIT:
+           game = False
+  
+   if finish != True:
+       window.blit(background,(0, 0))
+       player.update()
+       monster.update()
+      
+       player.reset()
+       monster.reset()
+       final.reset()
+
+       wall_1.draw_wall()
+       wall_2.draw_wall()
+       wall_3.draw_wall()
+
+   if sprite.collide_rect(player, monster) or sprite.collide_rect(player, wall_1) or sprite.collide_rect(player, wall_2) or sprite.collide_rect(player, wall_3):
+        finish = True
+        window.blit(lose, (200, 200))
+
+   if sprite.collide_rect(player, final):
+        finish = True
+        window.blit(win, (200, 200))
+        money.play()
+
+ 
+   display.update()
+   clock.tick(FPS)
